d07927110869077f127007b014066ec55cc39be6
Hai Pham <ngochai285nd@gmail.com>
2023-11-01 07:31:27 +0700

    init project

---
+++ src/main/java/com/haiphamcoder/cacheaside/demo/DemoCacheAsideWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.cacheaside.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCacheAsideWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCacheAsideWithSpringBootApplication.class, args);
+    }
+
+}
